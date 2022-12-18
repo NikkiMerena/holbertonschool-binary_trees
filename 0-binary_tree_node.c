ccccad1481e3fb0d2f0061888c338359364d349a
NikkiMerena <alderman.nikki1022@gmail.com>
2022-12-18 08:04:56 -0800

    updated errors

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,25 +1,24 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - creates a new binary tree node
- * @parent: Pointer to the parent node
- * @value: Value to store in the new node
+ * binary_tree_node - creates a new node for a binary tree
+ * @parent: parent node of the one to be created
+ * @value: integer  value it is to be given
  *
- * Return: Pointer to the new node
- */
-
+ * Return: pointer to the new node.
+*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-        binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
 	new_node = malloc(sizeof(binary_tree_t));
-        if (new_node == NULL)
-                return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
-        new_node->n = value;
-        new_node->parent = parent;
-        new_node->left = NULL;
-        new_node->right = NULL;
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-        return (new_node);
+	return (new_node);
 }
